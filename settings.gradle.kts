7078eb7dd369885612b183ad8a62bd132c9b9506
Robert Novotny <robert.novotny@jetbrains.com>
2026-05-07 17:18:27 +0200

    Infra: Add CSS to top-level Gradle build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "html-js-react-css-psi-showcase"
 includeBuild("html-code-inspection")
 includeBuild("js-function-line-marker")
 includeBuild("react-jsx-structure")
+includeBuild("css-psi-action")
