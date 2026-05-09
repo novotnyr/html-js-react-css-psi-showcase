8a4a90fe5df11b3ffef2710d7e3a12214fc047fe
Robert Novotny <robert.novotny@jetbrains.com>
2026-05-09 17:29:01 +0200

    HTML CSS: Remove comments and unnecessary build components

--- css-in-html-action/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-<!-- Keep a Changelog guide -> https://keepachangelog.com -->
-
-# Css-in-html-action Changelog
-
-## [Unreleased]
--- css-in-html-action/settings.gradle.kts
+++ css-in-html-action/settings.gradle.kts
@@ -5,7 +5,6 @@ rootProject.name = "css-in-html-action"
 pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version "2.2.20"
-        id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
@@ -16,11 +15,8 @@ plugins {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    // Configure all projects' repositories
     repositories {
         mavenCentral()
-
-        // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
         intellijPlatform {
             defaultRepositories()
         }
